19c6b3b131bd003bbf58bb6b23ef2918c498a78e
Daniel Luiz <156124027+danielluiz07@users.noreply.github.com>
2025-05-24 13:55:42 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post('/cadastrar', (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Servidor rodando na porta 3000');
 });
